f30f34a1ea24e8b0a2dff750f2df43e277f8869f
jessicamcg <jessicamcguico@gmail.com>
2021-10-28 09:44:22 -0500

    api/product get method good

--- models/index.js
+++ models/index.js
@@ -15,14 +15,20 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-// Product.belongsToMany(Tag, {
-
-// });
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false, 
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
-// Tag.belongsToMany(Product, {
-
-// });
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false, 
+  },
+});
 
 module.exports = {
   Product,
